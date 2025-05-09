700af2b7717f333addb11a360158bc376975e725
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-09 23:01:18 +0700

    allow nullable

--- modules/agenda/hooks/ag_kegiatan/before-create.php
+++ modules/agenda/hooks/ag_kegiatan/before-create.php
@@ -5,3 +5,8 @@ $_POST['pendamping'] = $data['pendamping'];
 unset($data['pendamping']);
 
 $data['instruksi'] = implode(',', $data['instruksi']);
+
+if(empty($data['pejabat_id']))
+{
+    unset($data['pejabat_id']);
+}
