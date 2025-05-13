591c971b2e4a939709e03bf95ea67860f0c8c6d9
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-13 11:21:32 +0700

    oke

--- themes/sneat/footer.php
+++ themes/sneat/footer.php
@@ -59,9 +59,12 @@
 ]; 
 foreach($js as $script )
 {
-	echo "<script src='$script'></script>";
+	echo "<script src=''></script>";
 }
 ?>
+
+<script src="<?= asset('theme/assets/js/datatables/datatables.bootstrap5.min.js') ?>"></script>
+<script src="<?= asset('theme/assets/js/datatables-pagingtype/full_numbers_no_ellipses.js') ?>"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js" integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <?php foot_script() ?>
