d0c0c183ffb18994a0c10e926e50b4d104cc3f75
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-13 15:41:13 +0700

    penambahan validasi no surat unik

--- modules/agenda/hooks/ag_surat/before-create.php
+++ modules/agenda/hooks/ag_surat/before-create.php
@@ -1,6 +1,14 @@
 <?php
 
 use Core\Storage;
+use Core\Validation;
+
+Validation::run([
+    'no_surat' => [
+        'required',
+        'unique:ag_surat'
+    ]
+], $data);
 
 $file = $_FILES['file_url'];
 $data['file_url'] = Storage::upload($file);
