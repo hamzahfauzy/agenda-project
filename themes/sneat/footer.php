3d8902de8ea5fb9ecc853388bec91a6bbb54f133
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-13 11:20:54 +0700

    update: oke

--- themes/sneat/footer.php
+++ themes/sneat/footer.php
@@ -49,11 +49,19 @@
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 
 <!-- Datatables -->
-<script src="https://cdn.datatables.net/2.3.0/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.js"></script>
-<script src="https://cdn.datatables.net/responsive/3.0.4/js/responsive.dataTables.js"></script>
-<script src="<?= asset('theme/assets/js/datatables/datatables.bootstrap5.min.js') ?>"></script>
-<script src="<?= asset('theme/assets/js/datatables-pagingtype/full_numbers_no_ellipses.js') ?>"></script>
+<?php $js = [
+	'https://code.jquery.com/jquery-3.7.1.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js',
+	'https://cdn.datatables.net/2.3.0/js/dataTables.js',
+	'https://cdn.datatables.net/2.3.0/js/dataTables.bootstrap5.js',
+	'https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.js',
+	'https://cdn.datatables.net/responsive/3.0.4/js/responsive.bootstrap5.js',
+]; 
+foreach($js as $script )
+{
+	echo "<script src='$script'></script>";
+}
+?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js" integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <?php foot_script() ?>
