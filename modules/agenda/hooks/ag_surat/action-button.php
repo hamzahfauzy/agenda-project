8e724f2947de59b0f3d7141f500585f9b546a675
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-13 15:18:28 +0700

    add eye on view btn

--- modules/agenda/hooks/ag_surat/action-button.php
+++ modules/agenda/hooks/ag_surat/action-button.php
@@ -6,7 +6,7 @@ $button = '';
 
 if(is_allowed(parsePath(routeTo('agenda/surat/view')), $user->id))
 {
-    $button .= '<a href="'.routeTo('agenda/surat/view', ['id' => $data->id]).'" class="btn btn-sm btn-info">Lihat<a> ';
+    $button .= '<a href="'.routeTo('agenda/surat/view', ['id' => $data->id]).'" class="btn btn-sm btn-info"><i class="fas fa-eye"></i> Lihat<a> ';
 }
 
 return $button;
