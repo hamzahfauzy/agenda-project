70c2974ba8e838970850efc1fa167ffcd157b58b
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-18 22:20:33 +0700

    update

--- modules/agenda/hooks/ag_kegiatan/edit-fields.php
+++ modules/agenda/hooks/ag_kegiatan/edit-fields.php
@@ -1,18 +1,23 @@
 <?php
 
-use Core\Database;
+// use Core\Database;
 
-$db = new Database;
-$pendamping = $db->all('ag_pendamping_kegiatan', [
-    'kegiatan_id' => $_GET['id']
-]);
+// $db = new Database;
+// $pendamping = $db->all('ag_pendamping_kegiatan', [
+//     'kegiatan_id' => $_GET['id']
+// ]);
 
-$ids = [];
-foreach($pendamping as $p)
-{
-    $ids[] = $p->pejabat_id;
-}
+// $ids = [];
+// foreach($pendamping as $p)
+// {
+//     $ids[] = $p->pejabat_id;
+// }
 
-$fields['pendamping']['attr']['value'] = $ids;
+// $fields['pendamping']['attr']['value'] = $ids;
+
+unset($fields['instruksi']);
+unset($fields['deskripsi']);
+unset($fields['pendamping']);
+unset($fields['pelaksana']);
 
 return $fields;
