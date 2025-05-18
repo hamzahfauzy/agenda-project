a3590e73ceed936830184d474f25725cfd145ef6
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-18 22:11:04 +0700

    oke fix

--- modules/agenda/hooks/ag_surat/index.php
+++ modules/agenda/hooks/ag_surat/index.php
@@ -22,7 +22,8 @@ if(hasRole($user->id, 'Ajudan'))
 
 $where = $where ." ". $having;
 
-$this->db->query = "SELECT * FROM $this->table $where ";
+$this->db->query = "SELECT $this->table.*,
+                            ag_kegiatan.tanggal tanggal_kegiatan FROM $this->table LEFT JOIN ag_kegiatan ON ag_kegiatan.surat_id = $this->table.id $where ";
 
 if(!in_array(get_role($user->id)->name, ['Admin','Super Admin']) && !hasRole($user->id, 'Ajudan'))
 {
@@ -33,13 +34,14 @@ if(!in_array(get_role($user->id)->name, ['Admin','Super Admin']) && !hasRole($us
                         LEFT JOIN ag_kegiatan ON ag_kegiatan.surat_id = $this->table.id
                         JOIN ag_surat_flow ON 
                             ag_surat_flow.surat_id = $this->table.id AND 
-                            ag_surat_flow.user_id = $user->id $where ";
+                            ag_surat_flow.user_id = $user->id 
+                        $where ";
 }
 
 $total = $this->db->exec('exists');
 
 $this->db->query .= "ORDER BY CASE 
-    WHEN ag_kegiatan.tanggal >= CURDATE() THEN 0
+    WHEN tanggal_kegiatan >= CURDATE() THEN 0
     ELSE 1
   END,
   ABS(DATEDIFF(tanggal_kegiatan, CURDATE())) LIMIT $start,$length";
