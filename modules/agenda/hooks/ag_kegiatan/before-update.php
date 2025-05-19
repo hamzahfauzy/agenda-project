e6bfb005c91dc07cce9f784b737937ec17a92dbd
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-20 00:03:46 +0700

    fix: before update

--- modules/agenda/hooks/ag_kegiatan/before-update.php
+++ modules/agenda/hooks/ag_kegiatan/before-update.php
@@ -3,8 +3,12 @@
 use Core\Database;
 
 $pendamping = $data['pendamping'];
+$pelaksana = $data['pelaksana'];
 
 unset($data['pendamping']);
+unset($data['pelaksana']);
+
+$data['instruksi'] = implode(',', $data['instruksi']);
 
 $db = new Database;
 $db->delete('ag_pendamping_kegiatan', [
@@ -15,6 +19,16 @@ foreach($pendamping as $p)
 {
     $db->insert('ag_pendamping_kegiatan', [
         'kegiatan_id' => $_GET['id'],
-        'pejabat_id' => $p
+        'pejabat_id' => $p,
+        'record_type' => 'PENDAMPING'
+    ]);
+}
+
+foreach($pelaksana as $p)
+{
+    $db->insert('ag_pendamping_kegiatan', [
+        'kegiatan_id' => $_GET['id'],
+        'pejabat_id' => $p,
+        'record_type' => 'PELAKSANA'
     ]);
 }
